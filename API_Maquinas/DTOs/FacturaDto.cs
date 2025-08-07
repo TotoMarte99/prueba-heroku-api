7a603914dd975b09d7da0cacad85049bcc8fd754
Tomas Martelon <86863400+TotoMarte99@users.noreply.github.com>
2025-08-07 22:08:47 +0200

    pdf sol

--- API_Maquinas/DTOs/FacturaDto.cs
+++ API_Maquinas/DTOs/FacturaDto.cs
@@ -3,14 +3,14 @@
     public class FacturaDto
     {
         public int VentaId { get; set; }
-        public string NombreCliente { get; set; }
-        public string ApellidoCliente { get; set; }
-        public string EmailCliente { get; set; }
+        public string? NombreCliente { get; set; }
+        public string? ApellidoCliente { get; set; }
+        public string?EmailCliente { get; set; }
         public DateTime FechaVenta { get; set; } = DateTime.UtcNow;
         public decimal TotalVenta { get; set; }
         public List<FacturaItemDto> Items { get; set; } = new List<FacturaItemDto>();
-        public string DireccionCliente { get; set; }
-        public string TelefonoCliente { get; set; }
+        public string? DireccionCliente { get; set; }
+        public string? TelefonoCliente { get; set; }
         
     }
 }
