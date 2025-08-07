bbe093ef5307d860d0ec5ba0d6af4ae11dfce75d
Tomas Martelon <86863400+TotoMarte99@users.noreply.github.com>
2025-08-07 23:44:57 +0200

    sol

--- API_Maquinas/DTOs/Ma_OrderDTO.cs
+++ API_Maquinas/DTOs/Ma_OrderDTO.cs
@@ -7,8 +7,8 @@
         // Datos del cliente
         public string Nombre { get; set; } = string.Empty;
         public string Apellido { get; set; } = string.Empty;
-        public string Telefono { get; set; } = string.Empty;
-        public string Email { get; set; } = string.Empty;
+        public string? Telefono { get; set; } 
+        public string? Email { get; set; } 
         public string Direccion { get; set; } = string.Empty;
 
         // Datos de la máquina
